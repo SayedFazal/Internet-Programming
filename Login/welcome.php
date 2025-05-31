add071f908636a7eb5ab9b80c020ddfe9aed900f
SayedFazal <syedfazal2956@gmail.com>
2025-05-31 09:18:52 +0530

    Create welcome.php

---
+++ Login/welcome.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+  header("Location: login.php");
+  exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head><title>Welcome</title></head>
+<body>
+  <h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
+  <a href="logout.php">Logout</a>
+</body>
+</html>
